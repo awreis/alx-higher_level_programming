7b0bb01bb078c260b7760610a7eb27a6739a7a50
Wareis <reiskofi@gmail.com>
2022-08-23 23:16:58 +0000

    aplh

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import string
-for letter in string.ascii_lowercase
-    if letter is not q and e
+for letter in string.ascii_lowercase:
+    if letter is not q and e:
         print("{}".format(letter))
