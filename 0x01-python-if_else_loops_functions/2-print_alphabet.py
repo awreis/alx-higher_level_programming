5bd0765de9dcfb1e51ec926025041234b7fe2433
Wareis <reiskofi@gmail.com>
2022-08-23 23:58:21 +0000

    error fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import string
 for letter in string.ascii_lowercase:
-    print("{0}".format(letter))
+    print(letter, end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 import string
 for letter in string.ascii_lowercase:
     if letter != "q" and letter != "e":
-        print("{}".format(letter))
+        print(letter, end="")
