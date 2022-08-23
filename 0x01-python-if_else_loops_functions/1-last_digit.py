0950a0d47126e83eff22aa844eb5abc8e2754f22
Wareis <reiskofi@gmail.com>
2022-08-23 21:01:21 +0000

    last digit

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print("Last digit of {} is {} and is ".format(number, n % 10))
+if (number % 10) > 5:
+    print("greater than 5\n")
+elif (number % 10) == 0:
+    print("0\n")
+elif (number % 10) < 6 and not 0:
+    print("less than 6 and not 0\n")
