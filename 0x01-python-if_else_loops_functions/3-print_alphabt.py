0f6d2817c445c57176748c410826ef7ac947d8c1
Wareis <reiskofi@gmail.com>
2022-08-23 23:31:38 +0000

    error fix

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import string
 for letter in string.ascii_lowercase:
-    if letter is not "q" and "e":
+    if letter != "q" and letter != "e":
         print("{}".format(letter))
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-hex() function
 for i in range(0, 99):
     print("{} = {}".format(i, hex(i)))
     
