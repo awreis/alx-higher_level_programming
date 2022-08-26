9813a5dc731c3256773541287f17c3b1c70293bc
Wareis <reiskofi@gmail.com>
2022-08-26 03:38:35 +0000

    fixed error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,10 +3,10 @@ if __name__ == "__main__"
     import sys
 
     count = len(sys.argv) - 1
-    if count == 1:
-        print("1 argument:")
-    elif count == 0:
+    if count == 0:
         print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
     else:
         print("{} arguments:".format(count))
     for i in range(count):
