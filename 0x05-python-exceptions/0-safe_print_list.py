b00ec8fb75f49eb3937bdd5c0ca15fd3b665794f
Wareis <reiskofi@gmail.com>
2022-09-12 12:46:48 +0000

    Error fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print('{:d}'.format(my_list[i]), end="")
             count += 1
     except:
-        pass
-    print()
-    return count
+        print("Oops... An error occured")
+        return count
 
