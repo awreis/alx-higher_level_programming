b80c008ce437d9ad17676593525e60c4b1dd07ac
awreis <40414863+awreis@users.noreply.github.com>
2022-09-14 04:23:58 +0000

    Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Define class Square with private size and public area
+Can access and update size
+"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """
+        Initialize a new square
+        Args:
+            size (int): The size of the new square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """O and return size"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Set value of size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+        def area(self):
+            """Return the current area of the square."""
+            return (self.__size * self.__size)
