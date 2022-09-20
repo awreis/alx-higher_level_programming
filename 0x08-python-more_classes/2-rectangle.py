a00a0c4799c4c31a020db73d3e24922d13ed1df1
Wareis <reiskofi@gmail.com>
2022-09-20 01:25:31 +0000

    Error fix

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -69,6 +69,6 @@ class Rectangle:
             Perimeter of the rectangle, as the sum of the height and
             width times 2
         """
-        if self.__width == 0 or self.__height == 0
+        if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * (self.__width + self.__height)
