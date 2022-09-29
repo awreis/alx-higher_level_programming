343e0fa66aa8a0e6c72d3586be85a567ef3d32ac
awreis <40414863+awreis@users.noreply.github.com>
2022-09-29 03:39:32 +0000

    Error fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,19 +1,17 @@
-#1/usr/bin/python3
+#!/usr/bin/python3
 """
-Finds if the object is an instance of a class that inherited
-(directly or indirectly) from the specified class
+Module 4-inherits_from.py
+Contains method inherits_from
+returns True if obj is instance of class that it inherits from or is subcls of
 """
-
 
 def inherits_from(obj, a_class):
-    """Determines if obj is an instance of a class that
-    inherited from a_class.
-
-    Args:
-        obj: object to look at
-        a_class: class to evaluate
-
-        Returns: True or False
     """
-
-    return isinstance(obj, a_class) and type(obj) != a_class
+    Notes:
+        use type() to get specific class
+        use isinstance() to get class and any parent classes too
+        use issubclass() to get what object is a subclass of
+    Return:
+        True if obj is instance of class that it inherits from or is subcls of
+    """
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
